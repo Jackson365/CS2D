2cae60ecf2693402d8f1b7692bde4607942498c2
Bruna Mirelly Ferreira Paixão <20211170150070@ifrn.local>
2024-01-05 13:05:24 -0300

    ...

--- CounterStrike-2D/Assets/Scripts/EnemyPatrulia.cs
+++ CounterStrike-2D/Assets/Scripts/EnemyPatrulia.cs
@@ -5,17 +5,15 @@ public class EnemyPatrulia : MonoBehaviour
     // Start is called before the first frame update
 
     public float Velocidade;
-    public Transform posicao;
+    public Transform[] posicao;
     public float TempoEspera;
 
-    public float xmin, xmax, ymin, ymax;
-
-    
+    int Randomica;
     private float Tempo;
-    
+  
     void Start()
     {
-        posicao.position = new Vector2(Random.Range(xmin, xmax), Random.Range(ymin, ymax));
+        Randomica = Random.Range(0, posicao.Length);
         Tempo = TempoEspera;
     }
 
@@ -23,18 +21,18 @@ public class EnemyPatrulia : MonoBehaviour
     void Update()
     {
         transform.position =
-            Vector2.MoveTowards(transform.position, posicao.position, Velocidade * Time.deltaTime);
+            Vector2.MoveTowards(transform.position, posicao[Randomica].position, Velocidade * Time.deltaTime);
 
-        float _dist = Vector2.Distance(transform.position, posicao.position);
+        float _dist = Vector2.Distance(transform.position, posicao[Randomica].position);
 
         if (_dist <- .2f)
         {
-            
+          
         }
 
         if (Tempo <= 0)
         {
-            posicao.position = new Vector2(Random.Range(xmin, xmax), Random.Range(ymin, ymax));
+            Randomica = Random.Range(0, posicao.Length);
             Tempo = TempoEspera;
         }
         else
